40c55c73a31962f43e78bcfd4dfccd37b051c321
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2019-02-01 22:58:13 +0700

    commit
    commit T 4 and  T5

--- tutorial-3/exercise/gradient-descent-exercise/app/gradient_descent_exercise.py
+++ tutorial-3/exercise/gradient-descent-exercise/app/gradient_descent_exercise.py
@@ -1,28 +1,52 @@
 import numpy as np
 import pandas as pd
+import math
+import sklearn.linear_model
+from sklearn.linear_model import LinearRegression
+
 
 def gradient_descent(x,y):
     m_curr=b_curr=0
-    iteration=10000
+    iteration=1000000
     n=len(x)
     learning_rate=0.0002
-
+    cost_pre=0
+    m_pre=0
+    b_pre=0
     for i in range(iteration):
         y_predicted=m_curr*x +b_curr
         cost=(1/n)*sum([val**2 for val in (y-y_predicted)])
-
         md=-(2/n)*sum(x*(y-y_predicted))
         bd=-(2/n)*sum(y-y_predicted)
 
         m_curr=m_curr - learning_rate*md
         b_curr=b_curr - learning_rate*bd
 
-        print("iteration:{}, m:{}, b:{}, cost:{}".format(i,m_curr,b_curr,cost))
+        if(i==0):
+            cost_pre=cost
+            m_pre=m_curr
+            b_pre=b_curr
+        if(i>0):
+            if(math.isclose(cost,cost_pre,rel_tol=1e-20)):
+                print("iteration:{}, m:{}, b:{}, cost:{}".format(i,m_pre,b_pre,cost_pre))
+                print("iteration:{}, m:{}, b:{}, cost:{}".format(i,m_curr,m_curr,cost))
+                break
+            else:
+                cost_pre=cost
+                m_pre=m_curr
+                b_pre=b_curr
+
+def linear_regresion(x,y):
+    lr=LinearRegression()
+    lr.fit(x,y);
+    return lr
+
+
 
 
 
 df=pd.read_csv("test_scores.csv")
-print(df)
+# print(df)
 
 df=df.drop(['name'],axis=1)
 
@@ -35,3 +59,7 @@ y_cs=np.array(df['cs'].values)
 # print(type(x_math))
 
 gradient_descent(x_math,y_cs)
+x_math=[x_math]
+lr=linear_regresion(df[['math']],df['cs'])
+print(lr.coef_)
+print(lr.intercept_)
